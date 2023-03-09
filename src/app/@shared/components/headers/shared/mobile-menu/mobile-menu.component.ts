4aaf6f6405a7838b0c8ac770d10f0fb8b13eca8a
fjpimienta <fjpimienta@gmail.com>
2023-03-09 17:33:36 -0600

    Shop: Ajuste en Menu Mobile.

--- src/app/@shared/components/headers/shared/mobile-menu/mobile-menu.component.ts
+++ src/app/@shared/components/headers/shared/mobile-menu/mobile-menu.component.ts
@@ -1,64 +1,91 @@
 import { ChangeDetectionStrategy, Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, NavigationStart } from '@angular/router';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { Catalog } from '@core/models/catalog.models';
+import { BrandsService } from '@core/services/brand.service';
+import { CategoriesService } from '@core/services/categorie.service';
 import { Subscription } from 'rxjs';
 
 declare var $: any;
 
 @Component({
-	selector: 'app-mobile-menu',
-	templateUrl: './mobile-menu.component.html',
-	styleUrls: ['./mobile-menu.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-mobile-menu',
+  templateUrl: './mobile-menu.component.html',
+  styleUrls: ['./mobile-menu.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class MobileMenuComponent implements OnInit, OnDestroy {
 
-	searchTerm = "";
+  searchTerm = '';
+  current = '/';
+  brands: Catalog[];
+  categories: Catalog[];
 
-	private subscr: Subscription;
+  private subscr: Subscription;
 
-	constructor(private router: Router) {
-		this.subscr = this.router.events.subscribe(event => {
-			if (event instanceof NavigationStart) {
-				this.hideMobileMenu();
-			}
-		});
-	}
+  constructor(
+    private router: Router,
+    public brandsService: BrandsService,
+    public categoriesService: CategoriesService
+  ) {
+    this.subscr = this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.hideMobileMenu();
+        this.current = event.url;
+      } else if (event instanceof NavigationEnd) {
+        this.current = event.url;
+      }
+    });
+    this.brands = [];
+    this.brandsService.getBrands(1, -1).subscribe(result => {
+      this.brands = result.brands;
+    });
+    this.categories = [];
+    this.categoriesService.getCategories(1, -1).subscribe(result => {
+      result.categories.forEach(cat => {
+        cat.param = {
+          category: cat.slug,
+          description: cat.description
+        };
+      });
+      this.categories = result.categories;
+    });
+  }
 
-	ngOnInit(): void {
-	}
+  ngOnInit(): void {
+  }
 
-	ngOnDestroy(): void {
-		this.subscr.unsubscribe();
-	}
+  ngOnDestroy(): void {
+    this.subscr.unsubscribe();
+  }
 
-	submenuToggle(e) {
-		const parent: HTMLElement = e.target.closest('li');
-		const submenu: HTMLElement = parent.querySelector('ul');
+  submenuToggle(e): void {
+    const parent: HTMLElement = e.target.closest('li');
+    const submenu: HTMLElement = parent.querySelector('ul');
 
-		if (parent.classList.contains('open')) {
-			$(submenu).slideUp(300, function () {
-				parent.classList.remove('open');
-			});
-		}
-		else {
-			$(submenu).slideDown(300, function () {
-				parent.classList.add('open');
-			});
-		}
+    if (parent.classList.contains('open')) {
+      $(submenu).slideUp(300, () => {
+        parent.classList.remove('open');
+      });
+    }
+    else {
+      $(submenu).slideDown(300, () => {
+        parent.classList.add('open');
+      });
+    }
 
-		e.preventDefault();
-		e.stopPropagation();
-	}
+    e.preventDefault();
+    e.stopPropagation();
+  }
 
-	hideMobileMenu() {
-		document.querySelector('body').classList.remove('mmenu-active');
-		document.querySelector('html').removeAttribute('style');
-	}
+  hideMobileMenu(): void {
+    document.querySelector('body').classList.remove('mmenu-active');
+    document.querySelector('html').removeAttribute('style');
+  }
 
-	submitSearchForm(e: any) {
-		e.preventDefault();
-		this.searchTerm = e.currentTarget.querySelector('.form-control').value;
-		this.router.navigate(['/shop/sidebar/list'], { queryParams: { searchTerm: this.searchTerm } });
-	}
+  submitSearchForm(e: any): void {
+    e.preventDefault();
+    this.searchTerm = e.currentTarget.querySelector('.form-control').value;
+    this.router.navigate(['/shop/sidebar/list'], { queryParams: { searchTerm: this.searchTerm } });
+  }
 }
