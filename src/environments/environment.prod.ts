57fce09099d0fe575b1ed6b37f11effab88c3624
fjpimienta <fjpimienta@gmail.com>
2023-07-10 19:13:44 -0600

    Shop: Configuracion Cambiar puerto para https.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  backend: 'https://189.253.186.95:8080/graphql',
-  backendWs: 'wss://189.253.186.95:8080/graphql',
+  backend: 'https://189.253.186.95:443/graphql',
+  backendWs: 'wss://189.253.186.95:443/graphql',
   stripePublicKey: 'pk_test_51LcAKpKmpYSfXVu3pcVivi8Yb5D9Rm359Uzl94jUsJgtzrHxyQHUyZPgyGqrwTLje73HwDrxRQZzbvQFVkhS534g004XdS4vSN',
-  upload: 'https://189.253.186.95:8080/uploads',
-  uploadsUrl: 'https://189.253.186.95:8080/uploads',
+  upload: 'https://189.253.186.95:443/uploads',
+  uploadsUrl: 'https://189.253.186.95:443/uploads',
   demo: 'demo30',
   SERVER_URL: ''
 };
