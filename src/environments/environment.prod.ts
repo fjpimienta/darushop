43c5bda5cb23286d93cb1c7dd4004537f8de1f7e
fjpimienta <fjpimienta@gmail.com>
2023-07-12 06:43:56 -0600

    Shop: Configuraciones acceso API Graphql

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  backend: 'http://189.253.186.95:443/graphql',
-  backendWs: 'ws://189.253.186.95:443/graphql',
+  backend: 'https://189.253.186.95:443/graphql',
+  backendWs: 'wss://189.253.186.95:443/graphql',
   stripePublicKey: 'pk_test_51LcAKpKmpYSfXVu3pcVivi8Yb5D9Rm359Uzl94jUsJgtzrHxyQHUyZPgyGqrwTLje73HwDrxRQZzbvQFVkhS534g004XdS4vSN',
-  upload: 'http://189.253.186.95:443/uploads',
-  uploadsUrl: 'http://189.253.186.95:443/uploads',
+  upload: 'https://189.253.186.95:443/uploads',
+  uploadsUrl: 'https://189.253.186.95:443/uploads',
   demo: 'demo30',
   SERVER_URL: ''
 };
