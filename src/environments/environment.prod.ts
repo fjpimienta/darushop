d30c11cea679bd1936e9aef3709e32618e5a1795
fjpimienta <fjpimienta@gmail.com>
2023-07-13 06:41:40 -0600

    Shop: Configuraciones para Productivo.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  backend: 'https://apidev.daru.mx:3001/graphql',
-  backendWs: 'wss://apidev.daru.mx:3001/graphql',
+  backend: 'https://api.daru.mx:3000/graphql',
+  backendWs: 'wss://api.daru.mx:3000/graphql',
   stripePublicKey: 'pk_test_51LcAKpKmpYSfXVu3pcVivi8Yb5D9Rm359Uzl94jUsJgtzrHxyQHUyZPgyGqrwTLje73HwDrxRQZzbvQFVkhS534g004XdS4vSN',
-  upload: 'https://apidev.daru.mx:3001/uploads',
-  uploadsUrl: 'https://apidev.daru.mx:3001/uploads',
+  upload: 'https://api.daru.mx:3000/uploads',
+  uploadsUrl: 'https://api.daru.mx:3000/uploads',
   demo: 'demo30',
   SERVER_URL: ''
 };
