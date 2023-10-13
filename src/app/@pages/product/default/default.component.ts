846154332042b069ed52d9245f46ad6c378a8c84
fjpimienta <fjpimienta@gmail.com>
2023-10-12 18:48:51 -0600

    Shop: Fix Menu sidebar categorias y subcategorias.

--- src/app/@pages/product/default/default.component.ts
+++ src/app/@pages/product/default/default.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { Product } from '@shared/classes/product';
 import { ProductsService } from '@core/services/products.service';
+import { IcecatProductsService } from '@core/services/suppliers/icecat.service';
 
 @Component({
   selector: 'app-product-default-page',
@@ -22,11 +23,23 @@ export class DefaultPageComponent implements OnInit {
   constructor(
     private activeRoute: ActivatedRoute,
     public router: Router,
-    public productService: ProductsService
+    public productService: ProductsService,
+    public icecatProductsService: IcecatProductsService
   ) {
     activeRoute.params.subscribe(params => {
       this.loaded = false;
       this.productService.getProduct(params.slug).subscribe(result => {
+        console.log('result: ', result);
+        console.log('result.product.product.brand: ', result.product.product.brand);
+        console.log('result.product.product.partnumber: ', result.product.product.partnumber);
+        this.icecatProductsService.getIcecatProduct(
+          result.product.product.brand,
+          result.product.product.partnumber
+        ).subscribe(result => {
+          console.log('icecatProduct/result: ', result);
+          if (result !== null) {
+          }
+        });
         if (result === null) {
           this.router.navigate(['/pages/404']);
         }
