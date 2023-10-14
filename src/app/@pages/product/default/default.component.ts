77bf5acc46509bc430f7dcd047e7416c34456ed0
fjpimienta <fjpimienta@gmail.com>
2023-10-13 18:37:33 -0600

    Shop: Vista Detalle de Productos - Avance de homologacion de la vista.

--- src/app/@pages/product/default/default.component.ts
+++ src/app/@pages/product/default/default.component.ts
@@ -29,25 +29,10 @@ export class DefaultPageComponent implements OnInit {
     activeRoute.params.subscribe(params => {
       this.loaded = false;
       this.productService.getProduct(params.slug).subscribe(result => {
-        console.log('result: ', result);
-        console.log('result.product.product.brand: ', result.product.product.brand);
-        console.log('result.product.product.partnumber: ', result.product.product.partnumber);
-        this.icecatProductsService.getIcecatProduct(
-          result.product.product.brand,
-          result.product.product.partnumber
-        ).subscribe(result => {
-          console.log('icecatProduct/result: ', result);
-          if (result !== null) {
-          }
-        });
-        if (result === null) {
-          this.router.navigate(['/pages/404']);
-        }
         this.product = result.product.product;
         this.productName = result.product.product.sku;
         // this.prev = this.product;
         // this.next = this.product;
-
         // this.product = result.product;
         // this.prev = result.prevProduct;
         // this.next = result.nextProduct;
