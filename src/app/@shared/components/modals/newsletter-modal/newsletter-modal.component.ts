96d048e87aaa8d6b8ff0abce64937f3e2c8754a2
fjpimienta <fjpimienta@gmail.com>
2023-12-14 20:20:31 -0600

    Shop: Activar Newsletter en el home.

--- src/app/@shared/components/modals/newsletter-modal/newsletter-modal.component.ts
+++ src/app/@shared/components/modals/newsletter-modal/newsletter-modal.component.ts
@@ -4,28 +4,99 @@ import Cookie from 'js-cookie';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { environment } from 'src/environments/environment';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MailService } from '@core/services/mail.service';
+import { infoEventAlert } from '@shared/alert/alerts';
+import { IMail } from '@core/interfaces/mail.interface';
+import { TYPE_ALERT } from '@shared/alert/values.config';
 
 @Component({
-	selector: 'app-newsletter-modal',
-	templateUrl: './newsletter-modal.component.html',
-	styleUrls: ['./newsletter-modal.component.scss']
+  selector: 'app-newsletter-modal',
+  templateUrl: './newsletter-modal.component.html',
+  styleUrls: ['./newsletter-modal.component.scss']
 })
 
 export class NewsletterModalComponent implements OnInit {
 
-	checkState = false;
+  checkState = false;
+  formData: FormGroup;
 
-	constructor(private modalService: NgbActiveModal) { }
+  constructor(
+    private modalService: NgbActiveModal,
+    private formBuilder: FormBuilder,
+    private mailService: MailService
+  ) { }
 
-	ngOnInit(): void {
-	}
+  ngOnInit(): void {
+    this.formData = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email, this.customEmailValidator]]
+    });
+  }
 
-	changeCheck() {
-		this.checkState = !this.checkState;
-	}
+  customEmailValidator(control) {
+    const email = control.value;
+    if (!email) {
+      return null;
+    }
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return regex.test(email) ? null : { invalidEmail: true };
+  }
 
-	closeModal() {
-		this.modalService.dismiss();
-		this.checkState && Cookie.set(`hideNewsletter-${environment.demo}`, "true", { expires: 7 });
-	}
+
+  changeCheck() {
+    this.checkState = !this.checkState;
+  }
+
+  closeModal() {
+    this.modalService.dismiss();
+    this.checkState && Cookie.set(`hideNewsletter-${environment.demo}`, "true", { expires: 7 });
+  }
+
+  onSubmit() {
+    if (!this.formData.valid) {
+      infoEventAlert('Es necesario un correo electrónico.', '');
+      return;
+    }
+
+    const email = this.formData.controls.email.value;
+    const receiptEmailInt = 'marketplace@daru.mx';
+    const subjectInt = 'Dar de Alta a Usuario';
+    const html = `
+      <!DOCTYPE html>
+      <html lang="es">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Contacto del Sitio DARU.MX</title>
+      </head>
+      <body>
+        <div class="container">
+          <h2>Correo Enviado desde Proximamente</h2>
+          <p>Nos ha contactado ${email}, para ser agregado a la lista de contactos.</p>
+          <hr>
+          <p>Favor de atender la solicitud.</p>
+          <hr>
+          <p class="foot">
+          </p>
+        </div>
+      </body>
+      </html>
+      `;
+
+    const mail: IMail = {
+      to: receiptEmailInt,
+      subject: subjectInt,
+      html: html
+    };
+
+    this.mailService.send(mail).subscribe(
+      (response) => {
+        infoEventAlert('Correo electrónico enviado con éxito:', '', TYPE_ALERT.SUCCESS);
+        this.formData.controls.email.setValue('');
+      },
+      (error) => {
+        infoEventAlert('Error al enviar el correo electrónico:', '', TYPE_ALERT.ERROR);
+      }
+    );
+  }
 }
