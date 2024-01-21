6332a2dfa9d53fb9b82ce5ab91649281ecf98ae5
fjpimienta <fjpimienta@gmail.com>
2024-01-21 13:32:41 -0600

    Shop: Update apidev.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  backend: 'https://apidev.daru.mx:3001/graphql',
-  backendWs: 'wss://apidev.daru.mx:3001/graphql',
+  backend: 'https://apidev.daru.mx:3002/graphql',
+  backendWs: 'wss://apidev.daru.mx:3002/graphql',
   stripePublicKey: 'pk_test_51LcAKpKmpYSfXVu3pcVivi8Yb5D9Rm359Uzl94jUsJgtzrHxyQHUyZPgyGqrwTLje73HwDrxRQZzbvQFVkhS534g004XdS4vSN',
-  upload: 'https://apidev.daru.mx:3001/uploads',
-  uploadsUrl: 'https://apidev.daru.mx:3001/uploads',
+  upload: 'https://apidev.daru.mx:3002/uploads',
+  uploadsUrl: 'https://apidev.daru.mx:3002/uploads',
   demo: 'demo30',
   SERVER_URL: '',
   KEY_SECRET: 'DARU2023##!!..'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  backend: 'https://apidev.daru.mx:3001/graphql',
-  backendWs: 'wss://apidev.daru.mx:3001/graphql',
+  backend: 'https://apidev.daru.mx:3002/graphql',
+  backendWs: 'wss://apidev.daru.mx:3002/graphql',
   stripePublicKey: 'pk_test_51NYXA6CQE0nb8Ka8LtAclE1JaXZxjpgF7V7oO8Bab8NXVQhkQx5uF7qiWod8VZu2KB1CiI7Y2VKq412v0k8OoyDy00tSs0cZDB',
-  upload: 'https://apidev.daru.mx:3001/uploads',
-  uploadsUrl: 'https://apidev.daru.mx:3001/uploads',
+  upload: 'https://apidev.daru.mx:3002/uploads',
+  uploadsUrl: 'https://apidev.daru.mx:3002/uploads',
   demo: 'demo30',
   SERVER_URL: '',
   KEY_SECRET: 'DARU2023##!!..'
