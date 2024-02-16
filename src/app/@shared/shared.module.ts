2c36ce934fcc0ad8cae9544f4d67e8db87384c20
fjpimienta <fjpimienta@gmail.com>
2024-02-16 10:49:30 -0600

    Shop: Modele - Fix duplicity CountDownComponent

--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -5,8 +5,10 @@ import { TranslateModule } from '@ngx-translate/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { OwlModule } from 'angular-owl-carousel';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Header Element
+import { HeaderComponent } from './components/headers/header/header.component';
 import { CartMenuComponent } from './components/headers/shared/cart-menu/cart-menu.component';
 import { CompareMenuComponent } from './components/headers/shared/compare-menu/compare-menu.component';
 import { WishlistMenuComponent } from './components/headers/shared/wishlist-menu/wishlist-menu.component';
@@ -16,10 +18,7 @@ import { HeaderSearchComponent } from './components/headers/shared/header-search
 import { MobileButtonComponent } from './components/headers/shared/mobile-button/mobile-button.component';
 import { MobileMenuComponent } from './components/headers/shared/mobile-menu/mobile-menu.component';
 
-// Header Component
-import { HeaderComponent } from './components/headers/header/header.component';
-
-// // Product Component
+// Product Component
 import { ProductOneComponent } from './components/product/product-one/product-one.component';
 import { ProductTwoComponent } from './components/product/product-two/product-two.component';
 import { ProductThreeComponent } from './components/product/product-three/product-three.component';
@@ -48,7 +47,7 @@ import { QuantityInputComponent } from './components/quantity-input/quantity-inp
 import { CountDownComponent } from './components/count-down/count-down.component';
 import { CountToComponent } from './components/count-to/count-to.component';
 
-// // single use component
+// single use component
 import { QuickViewComponent } from './components/modals/quick-view/quick-view.component';
 import { QuickViewTwoComponent } from './components/modals/quick-view-two/quick-view-two.component';
 import { VideoModalComponent } from './components/modals/video-modal/video-modal.component';
@@ -57,7 +56,7 @@ import { LoginModalComponent } from './components/modals/login-modal/login-modal
 import { IsotopeGridComponent } from './components/isotope-grid/isotope-grid.component';
 import { ImageComponent } from './components/image/image.component';
 
-// // Custom Directives
+// Custom Directives
 import { BgParallaxDirective } from './directives/bg-parallax.directive';
 import { TabClickDirective } from './directives/custom-tab-click.directive';
 import { ProductHoverDirective } from './directives/product-hover.directive';
@@ -69,131 +68,128 @@ import { BraFilterPipe } from './pipes/bra-filter.pipe';
 import { AttrFilterPipe } from './pipes/attr-filter.pipe';
 import { SafeContentPipe } from './pipes/safe-content.pipe';
 
-// // Post Component
+// Post Component
 import { PostOneComponent } from './components/blog-post/post-one/post-one.component';
 import { PostTwoComponent } from './components/blog-post/post-two/post-two.component';
 import { PostThreeComponent } from './components/blog-post/post-three/post-three.component';
 import { PostFourComponent } from './components/blog-post/post-four/post-four.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DatePickerLegalAgeModule } from './calendar/date-picker-legal-age/date-picker-legal-age.module';
 
 @NgModule({
-    declarations: [
-        // header
-        CartMenuComponent,
-        CompareMenuComponent,
-        WishlistMenuComponent,
-        CategoryMenuComponent,
-        MainMenuComponent,
-        HeaderSearchComponent,
-        MobileButtonComponent,
-        MobileMenuComponent,
-        HeaderComponent,
-        FooterComponent,
-        // product
-        ProductOneComponent,
-        ProductTwoComponent,
-        ProductThreeComponent,
-        ProductFourComponent,
-        ProductFiveComponent,
-        ProductSixComponent,
-        ProductSevenComponent,
-        ProductEightComponent,
-        ProductNineComponent,
-        ProductTenComponent,
-        ProductElevenComponent,
-        ProductTwelveComponent,
-        // single-use components
-        BreadcrumbComponent,
-        PageHeaderComponent,
-        QuickViewComponent,
-        QuickViewTwoComponent,
-        NewsletterModalComponent,
-        LoginModalComponent,
-        VideoModalComponent,
-        QuantityInputComponent,
-        CountDownComponent,
-        AccordionComponent,
-        CardComponent,
-        PaginationComponent,
-        IsotopeGridComponent,
-        ImageComponent,
-        // directives
-        BgParallaxDirective,
-        TabClickDirective,
-        ProductHoverDirective,
-        ContentAnimDirective,
-        // pipes
-        CatFilterPipe,
-        BraFilterPipe,
-        AttrFilterPipe,
-        SafeContentPipe,
-        CountDownComponent,
-        // // blog-post
-        PostOneComponent,
-        PostTwoComponent,
-        PostThreeComponent,
-        PostFourComponent,
-        CountToComponent
-    ],
-    imports: [
-        CommonModule,
-        RouterModule,
-        NgbModule,
-        ReactiveFormsModule,
-        FormsModule,
-        TranslateModule,
-        OwlModule,
-        LazyLoadImageModule,
-        DatePickerLegalAgeModule,
-    ],
-    exports: [
-        // header
-        HeaderComponent,
-        // mobile-menus
-        MobileMenuComponent,
-        // footer
-        FooterComponent,
-        // products
-        ProductOneComponent,
-        ProductTwoComponent,
-        ProductThreeComponent,
-        ProductFourComponent,
-        ProductFiveComponent,
-        ProductSixComponent,
-        ProductSevenComponent,
-        ProductEightComponent,
-        ProductNineComponent,
-        ProductTenComponent,
-        ProductElevenComponent,
-        ProductTwelveComponent,
-        // // single-use components
-        BreadcrumbComponent,
-        PageHeaderComponent,
-        CountDownComponent,
-        CountToComponent,
-        AccordionComponent,
-        CardComponent,
-        PaginationComponent,
-        QuantityInputComponent,
-        IsotopeGridComponent,
-        ImageComponent,
-        // directives
-        BgParallaxDirective,
-        TabClickDirective,
-        ProductHoverDirective,
-        ContentAnimDirective,
-        // pipes
-        CatFilterPipe,
-        BraFilterPipe,
-        AttrFilterPipe,
-        SafeContentPipe,
-        // // blog-post
-        PostOneComponent,
-        PostTwoComponent,
-        PostThreeComponent,
-        PostFourComponent,
-    ]
+  declarations: [
+    // header
+    CartMenuComponent,
+    CompareMenuComponent,
+    WishlistMenuComponent,
+    CategoryMenuComponent,
+    MainMenuComponent,
+    HeaderSearchComponent,
+    MobileButtonComponent,
+    MobileMenuComponent,
+    HeaderComponent,
+    FooterComponent,
+    // product
+    ProductOneComponent,
+    ProductTwoComponent,
+    ProductThreeComponent,
+    ProductFourComponent,
+    ProductFiveComponent,
+    ProductSixComponent,
+    ProductSevenComponent,
+    ProductEightComponent,
+    ProductNineComponent,
+    ProductTenComponent,
+    ProductElevenComponent,
+    ProductTwelveComponent,
+    // single-use components
+    BreadcrumbComponent,
+    PageHeaderComponent,
+    QuickViewComponent,
+    QuickViewTwoComponent,
+    NewsletterModalComponent,
+    LoginModalComponent,
+    VideoModalComponent,
+    QuantityInputComponent,
+    AccordionComponent,
+    CardComponent,
+    PaginationComponent,
+    IsotopeGridComponent,
+    ImageComponent,
+    // directives
+    BgParallaxDirective,
+    TabClickDirective,
+    ProductHoverDirective,
+    ContentAnimDirective,
+    // pipes
+    CatFilterPipe,
+    BraFilterPipe,
+    AttrFilterPipe,
+    SafeContentPipe,
+    // blog-post
+    PostOneComponent,
+    PostTwoComponent,
+    PostThreeComponent,
+    PostFourComponent,
+    CountToComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule,
+    TranslateModule,
+    OwlModule,
+    LazyLoadImageModule,
+    DatePickerLegalAgeModule,
+  ],
+  exports: [
+    // header
+    HeaderComponent,
+    // mobile-menus
+    MobileMenuComponent,
+    // footer
+    FooterComponent,
+    // products
+    ProductOneComponent,
+    ProductTwoComponent,
+    ProductThreeComponent,
+    ProductFourComponent,
+    ProductFiveComponent,
+    ProductSixComponent,
+    ProductSevenComponent,
+    ProductEightComponent,
+    ProductNineComponent,
+    ProductTenComponent,
+    ProductElevenComponent,
+    ProductTwelveComponent,
+    // single-use components
+    BreadcrumbComponent,
+    PageHeaderComponent,
+    CountDownComponent,
+    CountToComponent,
+    AccordionComponent,
+    CardComponent,
+    PaginationComponent,
+    QuantityInputComponent,
+    IsotopeGridComponent,
+    ImageComponent,
+    // directives
+    BgParallaxDirective,
+    TabClickDirective,
+    ProductHoverDirective,
+    ContentAnimDirective,
+    // pipes
+    CatFilterPipe,
+    BraFilterPipe,
+    AttrFilterPipe,
+    SafeContentPipe,
+    // blog-post
+    PostOneComponent,
+    PostTwoComponent,
+    PostThreeComponent,
+    PostFourComponent,
+  ]
 })
 
 export class SharedModule { }
