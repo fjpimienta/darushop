4b10d5b5eaa551fda72230837ef9fe5af806ccbd
fjpimienta <fjpimienta@gmail.com>
2024-02-16 12:37:19 -0600

    Shop: Checkout - Production Variable Openpay

--- src/app/@pages/shop/checkout/checkout.component.ts
+++ src/app/@pages/shop/checkout/checkout.component.ts
@@ -541,7 +541,6 @@ export class CheckoutComponent implements OnInit, OnDestroy {
       this.deviceDataId = OpenPay.deviceData.setup("formData", "token_id");
       OpenPay.setId(environment.OPENPAY_MERCHANT_ID);
       OpenPay.setApiKey(environment.OPENPAY_CLIENT_SECRET);
-      OpenPay.setSandboxMode(true);
 
       // Observable para el cartItems
       this.cartService.cartItemsChanges$.subscribe((newValue) => {
