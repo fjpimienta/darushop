8ebb173b0a10400ed73778b481f9d2e266d99973
fjpimienta <fjpimienta@gmail.com>
2024-02-28 07:32:16 -0600

    Shop: Defautl Product - Fix product non-existent.

--- src/app/@pages/product/default/default.component.ts
+++ src/app/@pages/product/default/default.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from '@shared/classes/product';
 import { ProductsService } from '@core/services/products.service';
 import { IcecatProductsService } from '@core/services/suppliers/icecat.service';
+import { infoEventAlert } from '@shared/alert/alerts';
+import { TYPE_ALERT } from '@shared/alert/values.config';
 
 @Component({
   selector: 'app-product-default-page',
@@ -30,17 +32,22 @@ export class DefaultPageComponent implements OnInit {
     activeRoute.params.subscribe(params => {
       this.loaded = false;
       this.productService.getProduct(params.slug).subscribe(result => {
-        this.product = result.product.product;
-        this.productName = result.product.product.partnumber;
-        // this.prev = this.product;
-        // this.next = this.product;
-        // this.product = result.product;
-        // this.prev = result.prevProduct;
-        // this.next = result.nextProduct;
-        // this.related = result.relatedProducts;
-        this.loaded = true;
-        if (this.product.pictures.length >= 2) {
-          this.gallery_images = true;
+        if (result && result.product && result.product.product) {
+          this.product = result.product.product;
+          this.productName = result.product.product.partnumber;
+          // this.prev = this.product;
+          // this.next = this.product;
+          // this.product = result.product;
+          // this.prev = result.prevProduct;
+          // this.next = result.nextProduct;
+          // this.related = result.relatedProducts;
+          this.loaded = true;
+          if (this.product.pictures.length >= 2) {
+            this.gallery_images = true;
+          }
+        } else {
+          infoEventAlert(result.product.message, '', TYPE_ALERT.WARNING);
+          this.router.navigate(['/']);
         }
       });
     });
