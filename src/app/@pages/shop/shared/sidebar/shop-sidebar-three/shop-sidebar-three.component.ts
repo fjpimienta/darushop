ea2f7b075d8267145f8bf7e70308ae5f453c75df
Francisco Pimienta <fjpimienta@gmail.com>
2024-09-19 06:37:26 -0600

    Shop: Filters Price

--- src/app/@pages/shop/shared/sidebar/shop-sidebar-three/shop-sidebar-three.component.ts
+++ src/app/@pages/shop/shared/sidebar/shop-sidebar-three/shop-sidebar-three.component.ts
@@ -253,9 +253,8 @@ export class ShopSidebarThreeComponent implements OnInit, OnChanges {
     if (event.currentTarget.checked) {
       queryParams = { ...queryParams, minPrice: value.min, maxPrice: value.max, page: 1 };
     } else {
-      queryParams = { ...queryParams, minPrice: 0, maxPrice: 9999, page: 1 };
+      queryParams = { ...queryParams, minPrice: 0, maxPrice: 999999, page: 1 };
     }
-
     this.router.navigate([], { queryParams, queryParamsHandling: 'merge' });
   }
 }
